0a5a3e1d70fd3591ccf1911c5b10815fe139300c
stanchanpsu <stanchanpsu@outlook.com>
2019-01-25 00:05:44 -0500

    Fix typo

--- lib/src/material_color_picker.dart
+++ lib/src/material_color_picker.dart
@@ -96,7 +96,7 @@ class _MaterialColorPickerState extends State<MaterialColorPicker> {
       _isMainSelection = false;
     });
     if (widget.onMainColorChange != null) widget.onMainColorChange(color);
-    if (widget.allowShades && widget.onColorChange != null) widget.onColorchange(shadeColor);
+    if (widget.allowShades && widget.onColorChange != null) widget.onColorChange(shadeColor);
   }
 
   void _onShadeColorSelected(Color color) {
