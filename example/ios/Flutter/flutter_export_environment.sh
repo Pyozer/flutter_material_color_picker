a44fe7856a9fddd54b9dc280048991f79aa9d238
Jean-Charles Moussé <jeancharles.msse@gmail.com>
2021-08-08 16:17:41 +0200

    Re-create projet to update all secondary files. Example app can now be run on web.

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\bwolf\fvm\default"
-export "FLUTTER_APPLICATION_PATH=D:\git_libs\flutter_material_color_picker\example"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/podpak/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/podpak/Projects/flutter_material_color_picker/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
-export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=C:\Users\bwolf\fvm\default\bin\cache\artifacts\engine\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
